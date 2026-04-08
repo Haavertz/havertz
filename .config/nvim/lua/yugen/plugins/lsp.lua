2673b22f102e891f9fecf7904e569ccdaf5b33f9
Gleison <gleisonmorais06@gmail.com>
2026-04-08 01:48:23 +0000

    add: pairs

--- .config/nvim/lua/yugen/plugins/lsp.lua
+++ .config/nvim/lua/yugen/plugins/lsp.lua
@@ -157,7 +157,6 @@ return {
     event = "InsertEnter",
     config = true,
     opts = {
-      disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
       disable_in_macro = true, -- disable when recording or executing a macro
       disable_in_visualblock = false, -- disable when insert after visual block mode
       disable_in_replace_mode = true,
@@ -165,8 +164,8 @@ return {
       enable_moveright = true,
       enable_afterquote = true, -- add bracket pairs after quote
       enable_check_bracket_line = true, --- check bracket in same line
-      enable_bracket_in_quote = true, 
-      enable_abbr = true, -- trigger abbreviation
+      enable_bracket_in_quote = true, --
+      enable_abbr = false, -- trigger abbreviation
       break_undo = true, -- switch for basic rule break undo sequence
       check_ts = false,
       map_cr = true,
