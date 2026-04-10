fbafd7275b16dd59291d567da4766bbc6e933a1a
Gleison <gleisonmorais06@gmail.com>
2026-04-10 11:46:02 -0400

    refactor: refatorando rose-pine adicionando himalaya mail e refatorando alpha

--- .config/nvim/lua/yugen/plugins/himalaya.lua
+++ .config/nvim/lua/yugen/plugins/himalaya.lua
@@ -4,8 +4,36 @@ return {
     "MunifTanjim/nui.nvim",
   },
   opts = {
-    -- Optional configuration
-    icons_enabled = false, -- set to true to use nerd font icons
-    wrap_folder_navigation = true,
+    sidebar = {
+      width = 30,
+    },
+    split_ratio = 0.4, -- email list takes 40% when split
+    wrap_folder_navigation = true, -- wrap to first/last when navigating
+    icons_enabled = true,
+    keymaps = {
+      listing = {
+        ["gq"] = "close",
+        ["]f"] = "next_folder",
+        ["[f"] = "previous_folder",
+        ["gF"] = "folder_picker",
+
+        ["<leader>r"] = function()
+          require("himalaya.folder").reload()
+          vim.notify("Reloaded!", vim.log.levels.INFO)
+        end,
+
+        ["<Tab>"] = "next_page",
+        ["<S-Tab>"] = "previous_page",
+
+        ["h"] = "previous_page",
+        ["l"] = "next_page",
+
+        ["<CR>"] = "open_email",
+      },
+
+      email = {
+        ["q"] = "close_email",
+      },
+    }
   },
 }
